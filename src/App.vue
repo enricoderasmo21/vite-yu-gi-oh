9df3d58a07ff1506a58ca2bf1ad8cb939d189dc5
enricoderasmo21 <derasmo.enrico@gmail.com>
2023-03-28 19:49:08 +0200

    Created AppNav

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <script>
+import AppNav from './components/AppNav.vue';
+
+export default{
+    data() {
+        return{
+
+        }
+    },
+    
+    components: {
+        AppNav,
+        
+    }
+}
 
 </script>
 
 <template>
- 
+    <AppNav></AppNav>
 </template>
 
 <style scoped>
---
+++ src/components/AppNav.vue
@@ -0,0 +1,41 @@
+<script>
+export default{
+    data() {
+        return{
+
+        }
+    }
+}
+</script>
+
+<template>
+    <nav>
+        <div id="container">
+            <div id="logo-container">
+                <img src="/img/logo.svg" alt="">
+            </div>
+        </div>
+    </nav>
+</template>
+
+<style scoped lang="scss">
+#container{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    height: 250px;
+
+    background-color: #383635;
+
+    box-shadow: 10px 15px 20px rgba(0,0,0,.6);
+
+    #logo-container{
+        width: 40%;
+
+        img{
+            width: 100%;
+        }
+    }
+}
+</style>
